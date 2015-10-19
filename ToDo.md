09be053822a552f187717a9e4f6f394acd37ce4c
Michael Ericson <ericson.michael.j@gmail.com>
2015-10-19 16:05:25 -0700

    First iteration complete, proof of concepts exists

--- ToDo.md
+++ ToDo.md
@@ -1,7 +1,7 @@
 First Iteration:
 
-[ ] - Set up your html files
-[ ] - Install dependencies - Angular, Jquery
+[x] - Set up your html files
+[x] - Install dependencies - Jquery
 [ ] - Create your client-side javascript file. In it:
-	[ ] - On submit, create an object
+	[x] - On submit, push into object
 	[ ] - On search, once clicked it was search via variable name
---
+++ client/app.js
@@ -0,0 +1,32 @@
+var equations = {};
+
+$(function() {
+    $(".submit_equation").on('click',function(){
+    	var $variables = $('.variable');
+    	var equation = $('.equation').val(); 
+    	for(var i = 0;i<$variables.length;i++){
+    		var variable_value = $($variables[i]).val()
+    		if(variable_value){
+	    		if(!equations[variable_value]){
+	    			var object = {};
+	    			object[equation] = true;
+	    			equations[variable_value] = object;
+	    		}else{
+	    			if(!equations[variable_value][equation]){
+	    				equations[variable_value][equation] = true;
+	    			}
+	    		}
+    		}
+    	}
+    	console.log("Final equations",equations);
+    })
+    $(".equation_search").on('click',function(){
+    	var variable = $('.equation_find').val();
+    	$("#equations").html("")
+    	for (var equation in equations[variable]){
+    		//console.log(equation)
+
+    		$("#equations").append("<p>" + equation + "</p>");
+    	}
+    })
+});
